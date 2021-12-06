aff306f5402a724c5d9329bc23006779d5b021f8
Erik Heemskerk <heemskerkerik@users.noreply.github.com>
2021-12-06 14:42:44 +0100

    Make code even nicer

--- fsharp06/Program.fs
+++ fsharp06/Program.fs
@@ -1,5 +1,8 @@
 let input = Input.realInput
 let parsedInput = input.Split(',') |> Array.map int
+let initialBuckets = {0..8} |> Seq.map (fun i -> parsedInput |> Seq.filter ((=) i) |> Seq.length |> int64) |> Array.ofSeq
+
+let discard f a _ = f a
 
 let cycle (buckets: int64[]) =
     let spawningFishCount = buckets.[0]
@@ -8,17 +11,15 @@ let cycle (buckets: int64[]) =
             | 8 -> spawningFishCount
             | 6 -> buckets.[7] + spawningFishCount
             | _ -> buckets.[index + 1]
-
     [|0..8|] |> Array.map getNewValueForIndex
 
-let discard f a _ = f a
-
 let runSimulation cycleCount =
-    let buckets = {0..8} |> Seq.map (fun i -> parsedInput |> Seq.filter ((=) i) |> Seq.length |> int64) |> Array.ofSeq
-    let finalBuckets = {1..cycleCount} |> Seq.fold (discard cycle) buckets
+    let finalBuckets = {1..cycleCount} |> Seq.fold (discard cycle) initialBuckets
+    Array.sum finalBuckets
 
-    printfn $"Fish after %d{cycleCount} days: %d{Array.sum finalBuckets}"
+let runSimulationWithOutput cycleCount =
+    let fishCount = runSimulation cycleCount
 
-runSimulation 18
-runSimulation 80
-runSimulation 256
+    printfn $"Fish after %d{cycleCount} days: %d{fishCount}"
+
+runSimulationWithOutput 256
